7a07cbb56de47c9d47249337615324b4f8abfb1a
zhangjie <595759874@qq.com>
2020-01-20 16:37:11 +0800

    add pageSelect

---
+++ src/main/java/com/baomidou/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.baomidou.com.example.demo.config;
+
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan({"com.baomidou.mybatisplus.samples.quickstart.mapper","com.baomidou.com.example.demo.mapper"})
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
--- src/main/java/com/baomidou/com/example/demo/controller/AaController.java
+++ src/main/java/com/baomidou/com/example/demo/controller/AaController.java
@@ -2,13 +2,20 @@ package com.baomidou.com.example.demo.controller;
 
 
 import com.baomidou.com.example.demo.entity.Aa;
+import com.baomidou.com.example.demo.mapper.AaMapper;
 import com.baomidou.com.example.demo.service.IAaService;
+import com.baomidou.com.example.demo.service.impl.AaServiceImpl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sun.java2d.pipe.AAShapePipe;
 
 import java.sql.Wrapper;
+import java.util.List;
 
 /**
  * <p>
@@ -24,16 +31,32 @@ public class AaController {
 
     @Autowired
     IAaService iAaService;
+
+    @Autowired
+    AaMapper aaMapper;
+
     @PostMapping("/add")
-    public Boolean add(@RequestBody Aa aa){
+    public Boolean add(@RequestBody Aa aa) {
         return iAaService.save(aa);
     }
+
     @PostMapping("/update")
-    public void update(@RequestBody Aa aa){
+    public void update(@RequestBody Aa aa) {
         iAaService.updateById(aa);
     }
+
     @PostMapping("/selectOne")
-    public Aa selectOne(@RequestParam("id")Long id){
+    public Aa selectOne(@RequestParam("id") Long id) {
         return iAaService.getById(id);
+    }
+
+    @PostMapping("/selectList")
+    public List<Aa> selectList(@RequestParam(value = "page" ,required = false)Integer page,
+                               @RequestParam(value = "pageSize",required = false)Integer pageSize) {
+       // IPage<Aa> userPage = new Page<>(page, pageSize);//参数一是当前页，参数二是每页个数
+        IPage<Aa> userPage = new Page<>(1, 10);//参数一是当前页，参数二是每页个数
+        userPage = aaMapper.selectPage(userPage, null);
+        List<Aa> list = userPage.getRecords();
+        return list;
     }
 }
