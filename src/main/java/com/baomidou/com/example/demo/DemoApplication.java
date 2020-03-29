cc4aad3fc96a0230ecfa1718d49bb119c8369003
zhangjie <595759874@qq.com>
2020-03-29 22:27:55 +0800

    启动成功

--- src/main/java/com/baomidou/com/example/demo/DemoApplication.java
+++ src/main/java/com/baomidou/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.baomidou.com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SpringBootApplication
+@MapperScan({"com.baomidou.mybatisplus.samples.quickstart.mapper","com.baomidou.com.example.demo.mapper"})
 public class DemoApplication {
 
         public static void main(String[] args) {
-                SpringApplication.run(DemoApplication.class);
+                SpringApplication.run(DemoApplication.class,args);
         }
 
 
